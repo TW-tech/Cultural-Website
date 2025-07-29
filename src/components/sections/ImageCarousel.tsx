27b63e30a426c2f00d312f3e4aed057875233c07
Katy <surekaty0919@gmail.com>
2025-07-29 10:06:57 -0400

    fix proportion

--- src/components/sections/ImageCarousel.tsx
+++ src/components/sections/ImageCarousel.tsx
@@ -54,12 +54,10 @@ export default function ImageCarousel() {
 
   return (
     <section className="py-8 sm:py-10 lg:py-12 bg-[#FAF9EB]">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        
-        {/* 輪播容器 */}
-        <div className="relative max-w-6xl mx-auto group">
-          {/* 三圖輪播區域 - 中間主圖，左右預覽 */}
-          <div className="relative h-[20rem] sm:h-[16rem] lg:h-[24rem] flex items-center justify-center gap-4 lg:gap-8">
+      {/* 滿寬輪播容器 */}
+      <div className="relative w-full group">
+        {/* 三圖輪播區域 - 寬扁比例 */}
+        <div className="relative h-[20rem] sm:h-[18rem] lg:h-[26rem] xl:h-[28rem] 2xl:h-[30rem] flex items-center justify-center gap-4 lg:gap-8 px-4 sm:px-6 lg:px-8">
             
             {/* 左側預覽圖 */}
             <div className="hidden sm:block relative w-[18%] h-[60%] lg:h-[70%] opacity-60 hover:opacity-80 transition-all duration-300 cursor-pointer transform hover:scale-105">
@@ -162,7 +160,6 @@ export default function ImageCarousel() {
           </div>
 
         </div>
-      </div>
     </section>
   );
 }
